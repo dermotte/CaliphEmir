19871728731102732b8bf7d1265a2ac8e9817059
motte <motte@44850356-e34d-0410-8c29-e1380090ef48>
2010-11-09 15:56:44 +0000

    Added surf based local feature histograms.

---
+++ lire/src/main/java/net/semanticmetadata/lire/imageanalysis/Histogram.java
@@ -0,0 +1,12 @@
+package net.semanticmetadata.lire.imageanalysis;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mlux
+ * Date: 29.09.2010
+ * Time: 16:02:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Histogram {
+    public float[] descriptor = null;
+}
